2949bb2268ef74e21ac9f45856d34a7ad4b328b9
Manuel Araoz <manuelaraoz@gmail.com>
2015-05-18 11:21:23 -0300

    first version

--- index.js
+++ index.js
@@ -9,13 +9,48 @@ var Transaction = bitcore.Transaction;
 var Networks = bitcore.Networks;
 var network = Networks.livenet;
 
+var Insight = require('bitcore-explorers').Insight;
+var insight = new Insight();
+
 
 var peer = new Peer('localhost', network);
+var txcount = 0;
+
 peer.on('tx', function(m) {
-  console.log(m.transaction.id);
   var b = m.transaction.toBuffer();
-  console.log(b.toString('hex'));
+  for (var i = 0; i < b.length * 8; i++) {
+    // flip bit i
+    var copy = new Buffer(b.length);
+    b.copy(copy);
+    var j = Math.floor(i / 8);
+    var n = copy[j] ^ (1 << (7 - i % 8));
+    //console.log(i, j, copy[j], n);
+    copy[j] = n;
+    txcount += 1;
+    var pt, pts;
+    try {
+      pt = new Transaction(copy);
+      pts = pt.uncheckedSerialize();
+    } catch (exc) {
+      // nada
+    }
+    if (!pts) {
+      continue;
+    }
+    insight.broadcast(pts, function(err, txid) {
+      if (err) {
+        console.log('err', err);
+        return;
+      }
+      console.log(m.transaction.id, txid);
+    });
+  }
 });
+
+setInterval(function() {
+  console.log(txcount);
+}, 5000);
+
 peer.on('inv', function(m) {
   peer.sendMessage(Messages.GetData(m.inventory));
 });
